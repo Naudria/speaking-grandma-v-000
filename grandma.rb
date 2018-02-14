96f797b98ab638d7b6dd7834a5485861c6be460c
Rima Jean <rima.jean@gmail.com>
2018-02-14 16:07:46 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,15 +2,15 @@
 
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
-     return "I LOVE YOU TOO PUMPKIN!"
+     puts "I LOVE YOU TOO PUMPKIN!"
    elsif phrase == phrase.downcase
-   		return "HUH?! SPEAK UP, SONNY!"
+   		puts "HUH?! SPEAK UP, SONNY!"
     elsif phrase == phrase.downcase
-     return "HUH?! SPEAK UP, SONNY!"
+     puts "HUH?! SPEAK UP, SONNY!"
    elsif phrase == phrase.downcase
-     return "HUH?! SPEAK UP, SONNY!"
+     puts "HUH?! SPEAK UP, SONNY!"
      else
-   		return "NO, NOT SINCE 1938!"
+   		puts "NO, NOT SINCE 1938!"
      end
    end
 
